15b873c078491eda5b883bd8fafb334a659d6d7c
tracer-ics2016 <tracer@njuics.org>
2021-01-16 20:19:15 +0800

    >  compile NEMU 3019244095 abc Linux ubuntu 5.4.0-62-generic #70~18.04.1-Ubuntu SMP Tue Jan 12 17:18:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:19:15 up 46 min,  1 user,  load average: 0.42, 0.39, 0.35 ea0fedc38f9a03e146054f686073e16ece5c7a26

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -7,6 +7,25 @@ enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
+struct block
+{
+	bool valid;
+	uint32_t tag;
+	uint8_t *buf;
+};
+
+struct set
+{
+	struct block *blocks;
+};
+
+typedef struct
+{
+	int s, E, b;
+	struct set *sets;
+
+} Cache;
+
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
  * access cpu.gpr[3]._16, we will get the `bx' register; if we access
@@ -52,6 +71,7 @@ typedef struct {
 		};
 		uint32_t val;
 	} eflags;
+        Cache cache1;
 
 } CPU_state;
 
