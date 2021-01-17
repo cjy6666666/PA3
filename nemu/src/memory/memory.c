61bd153f82700bc4add6b4692f38b31608b206ae
tracer-ics2016 <tracer@njuics.org>
2021-01-17 10:38:12 +0800

    >  compile NEMU 3019244095 abc Linux ubuntu 5.4.0-62-generic #70~18.04.1-Ubuntu SMP Tue Jan 12 17:18:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:38:12 up 57 min,  1 user,  load average: 0.14, 0.14, 0.19 764a53606016c9f84ad5a6e9c0f6004cc023673d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -170,7 +170,6 @@ uint32_t read_cache2(hwaddr_t addr, size_t len, bool *flag)
 	off = addr & ((1 << cpu.cache2.b) - 1);
 	E = cpu.cache2.E;
 	set = &cpu.cache2.sets[index];
-	printf("Read\n");
 	for (i = 0; i < E && set->blocks[i].valid == true; i++)
 	{
 		if (set->blocks[i].tag == tag)
@@ -193,7 +192,6 @@ uint32_t read_cache2(hwaddr_t addr, size_t len, bool *flag)
 	{
 		set->blocks[i].valid = true;
 		set->blocks[i].tag = tag;
-		printf("P1:0x%x\n", addr - off);
 		for (j = 0; j < (1 << cpu.cache2.b); j++)
 		{
 			set->blocks[i].buf[j] = dram_read(addr - off + j, 1) & 255;
@@ -211,13 +209,11 @@ uint32_t read_cache2(hwaddr_t addr, size_t len, bool *flag)
 			set->blocks[i].dirty = false;
 		}
 		set->blocks[i].tag = tag;
-		printf("P2\n");
 		for (j = 0; j < (1 << cpu.cache2.b); j++)
 		{
 			set->blocks[i].buf[j] = dram_read(addr - off + j, 1) & 255;
 		}
 	}
-	printf("P3\n");
 	if (len == 4)
 		return *((uint32_t *)(set->blocks[i].buf + off));
 	if (len == 3)
